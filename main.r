9eb1c76f474f42bd5a86c92d05c20c4e02d43f6d
Alexandre DAVY <davyalexandr@gmail.com>
2023-02-02 22:03:23 -0500

    read-datasets

---
+++ main.r
@@ -0,0 +1,8 @@
+
+
+library(tidyverse) 
+library(dsbox) 
+
+budget <- read_csv("data/Budget_2022.csv")
+Major_Crime <- read_csv("data/Major_Crime_Indicators.csv")
+properties <- read_csv("data/properties.csv")
